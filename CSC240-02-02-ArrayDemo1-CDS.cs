8740766b6d91525af5c8c197371c04f2f3de17a0
csharr4183 <91540040+csharr4183@users.noreply.github.com>
2022-04-10 18:45:26 -0400

    Add files via upload

---
+++ CSC240-02-02-ArrayDemo1-CDS.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace ArrayDemo1
+{
+    class ArrayDemo1
+    {
+        static void Main()
+        {
+            double[] payrate = { 13.00, 17.35, 19.12, 22.45 };
+
+            for (int x = 0; x < payrate.Length; ++x)
+                WriteLine("Pay rate {0} is {1}",
+                    x, payrate[x].ToString("C"));
+
+
+        }
+    }
+}
